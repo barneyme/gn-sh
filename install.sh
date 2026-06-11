8167b4e8fb9fda12c6774217a2409bf870accab5
barneyme <barney@barney.me>
2026-06-10 20:55:03 -0400

    Add files via upload

--- install.sh
+++ install.sh
@@ -12,24 +12,17 @@ echo "Select your Git platform provider:"
 echo "1) GitHub (github.com)"
 echo "2) GitLab (gitlab.com)"
 echo "3) Codeberg (codeberg.org)"
-echo "4) Bitbucket (bitbucket.org)"
-read -rp "Enter choice [1-4, default 1]: " PROV_IDX
+read -rp "Enter choice [1-3, default 1]: " PROV_IDX
 PROV_IDX="${PROV_IDX:-1}"
 
 case "$PROV_IDX" in
     2) GIT_PROVIDER="gitlab" ;;
     3) GIT_PROVIDER="codeberg" ;;
-    4) GIT_PROVIDER="bitbucket" ;;
     *) GIT_PROVIDER="github" ;;
 esac
 
 echo ""
 echo "--- Configuring credentials for $GIT_PROVIDER ---"
-if [ "$GIT_PROVIDER" = "bitbucket" ]; then
-    echo "(Bitbucket uses an App Password, not a personal access token.)"
-    echo "Generate one at: Bitbucket > Settings > App passwords"
-    echo "Required permissions: Repositories (Read, Write)"
-fi
 read -rp "Personal Access Token / App Password: " GIT_TOKEN
 read -rp "Account Username:           " GIT_OWNER
 read -rp "Repository Name [gn]:       " GIT_REPO
